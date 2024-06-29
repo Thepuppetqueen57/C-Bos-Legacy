9bb2c113a11b9c46171a6ea72a7290da77628888
Puppet <122136792+Thepuppetqueen57@users.noreply.github.com>
2024-06-28 22:57:49 -0700

    Removed A LOT of bloat
    Not as messy anymore yay

--- libs/cboslib.py
+++ libs/cboslib.py
@@ -5,18 +5,6 @@ import ctypes
 import colorama
 import random
 
-
-def check_version(version):
-    response = requests.get(
-        "https://tps.puppet57.site/cbos/backend/versioncheck.php")
-    server_response = float(response.text)
-    if server_response > version:
-        return "Update available!"
-    elif server_response < version:
-        return "This is a beta!"
-    else:
-        return "No updates available!"
-
 def clear_console():
     if sys.platform.startswith('win'):
         os.system('cls')
@@ -36,14 +24,11 @@ def randomcolor():
         colorama.Fore.BLUE,
         colorama.Fore.MAGENTA,
         colorama.Fore.CYAN,
-        colorama.Fore.WHITE,
-        colorama.Fore.LIGHTBLACK_EX,
         colorama.Fore.LIGHTBLUE_EX,
         colorama.Fore.LIGHTCYAN_EX,
         colorama.Fore.LIGHTGREEN_EX,
         colorama.Fore.LIGHTMAGENTA_EX,
         colorama.Fore.LIGHTRED_EX,
-        colorama.Fore.LIGHTWHITE_EX,  # what
         colorama.Fore.LIGHTYELLOW_EX
     ]
     return random.choice(color_choices)
